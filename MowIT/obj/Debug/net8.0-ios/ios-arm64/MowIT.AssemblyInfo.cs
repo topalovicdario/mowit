8f4fdc9ffdd31092adc96dd14f7aa68fe9249445
Dario Topalović <dariotopalovic1212@gmail.com>
2026-06-08 18:18:04 +0200

    Updating specific factory of GreenTitan robot to match its implementation

--- MowIT/obj/Debug/net8.0-ios/ios-arm64/MowIT.AssemblyInfo.cs
+++ MowIT/obj/Debug/net8.0-ios/ios-arm64/MowIT.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MowIT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+7db38505aae52adcc98079634adbc165f917b04b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MowIT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MowIT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
