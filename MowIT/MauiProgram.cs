ec7ab193665c64e479892e46aa756db891043bb9
Dario Topalović <dariotopalovic1212@gmail.com>
2026-08-03 00:27:25 +0200

    Setting GreenTitan factory and updating platform configuration

--- MowIT/MauiProgram.cs
+++ MowIT/MauiProgram.cs
@@ -9,7 +9,10 @@ using MowIT.Domain.Strategies;
 using MowIT.Infrastructure;
 using MowIT.Infrastructure.Factories;
 using MowIT.Infrastructure.Persistence;
+using MowIT.Infrastructure.ScheduleSync;
 using MowIT.Infrastructure.Services;
+using MowIT.Infrastructure.Transport;
+using MowIT.Infrastructure.Wifi;
 using MowIT.Presentation.Pages;
 using MowIT.Presentation.ViewModels;
 
@@ -48,6 +51,8 @@ public static partial class MauiProgram
         var app = builder.Build();
 
 app.Services.GetRequiredService<MowingSchedulerService>();
+        app.Services.GetRequiredService<GeofenceMonitor>();
+        app.Services.GetRequiredService<GpsTraceService>();
 
         return app;
     }
@@ -57,7 +62,7 @@ private static void RegisterInfrastructure(IServiceCollection services)
         
         RegisterPlatformPermissions(services);
 
-if (RobotFactory is SimulatorServiceFactory)
+if (RobotFactory is SimulatorServiceFactory or MultiTransportFactory)
             services.AddSingleton<IBlePermissionService, NullBlePermissionService>();
 
 var dbPath = Path.Combine(
@@ -65,9 +70,39 @@ var dbPath = Path.Combine(
 
         services.AddSingleton(new AppDatabase(dbPath));
         services.AddSingleton<IBoundaryRepository, BoundaryRepository>();
-        services.AddSingleton<IScheduleRepository, ScheduleRepository>();
 
-services.AddSingleton(RobotFactory);
+        services.AddSingleton<ScheduleRepository>();
+
+        if (RobotFactory is MultiTransportFactory)
+        {
+            var wifi = new WifiRobotOptions();
+            var syncOpts = new ScheduleSyncOptions
+            {
+                BaseUrl     = wifi.BaseUrl,
+                RobotId     = wifi.RobotId,
+                BearerToken = wifi.BearerToken
+            };
+            services.AddSingleton<IScheduleSyncService>(sp =>
+                new HttpScheduleSyncService(
+                    new HttpClient(),
+                    Microsoft.Extensions.Options.Options.Create(syncOpts),
+                    sp.GetRequiredService<ILogger<HttpScheduleSyncService>>(),
+                    sp.GetRequiredService<MowIT.Application.Logging.EventLogService>()));
+        }
+        else
+        {
+            services.AddSingleton<IScheduleSyncService, NullScheduleSyncService>();
+        }
+
+        services.AddSingleton<IScheduleRepository>(sp =>
+            new SyncingScheduleRepository(
+                sp.GetRequiredService<ScheduleRepository>(),
+                sp.GetRequiredService<IScheduleSyncService>()));
+
+services.AddSingleton(new WifiRobotOptions());
+        services.AddSingleton<IRobotTransportSwitch, NullRobotTransportSwitch>();
+
+        services.AddSingleton(RobotFactory);
         RobotFactory.RegisterServices(services);
     }
 
@@ -80,14 +115,20 @@ private static void RegisterApplication(IServiceCollection services)
 
 services.AddSingleton<GeofenceMonitor>();
 
+        services.AddSingleton<GpsTraceService>();
+
 services.AddSingleton<MowingSchedulerService>();
 
 services.AddSingleton<IMessenger>(_ => WeakReferenceMessenger.Default);
 
 services.AddSingleton<MowIT.Application.Logging.EventLogService>();
 
+        services.AddSingleton<MowIT.Application.Services.LastMowSession>();
+
 services.AddTransient<IMowingStrategy, BoustrophedonStrategy>();
         services.AddTransient<IMowingStrategy, SpiralInwardStrategy>();
+
+        services.AddSingleton<MowIT.Application.Services.MowingRoutePlanner>();
     }
 
 private static void RegisterPresentation(IServiceCollection services)
@@ -95,17 +136,18 @@ private static void RegisterPresentation(IServiceCollection services)
         
         services.AddTransient<LoginPage>();
         services.AddTransient<ScanPage>();
-        services.AddTransient<DashboardPage>();
-        services.AddTransient<ControlPage>();
-        services.AddTransient<MapPage>();
-        services.AddTransient<SchedulePage>();
+
+        services.AddSingleton<DashboardPage>();
+        services.AddSingleton<ControlPage>();
+        services.AddSingleton<MapPage>();
+        services.AddSingleton<SchedulePage>();
 
 services.AddTransient<LoginViewModel>();
         services.AddTransient<ScanViewModel>();
-        services.AddTransient<DashboardViewModel>();
-        services.AddTransient<ControlViewModel>();
-        services.AddTransient<MapViewModel>();
-        services.AddTransient<ScheduleViewModel>();
+        services.AddSingleton<DashboardViewModel>();
+        services.AddSingleton<ControlViewModel>();
+        services.AddSingleton<MapViewModel>();
+        services.AddSingleton<ScheduleViewModel>();
     }
 
 static partial void RegisterPlatformPermissions(IServiceCollection services);
